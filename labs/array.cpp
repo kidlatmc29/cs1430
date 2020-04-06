38e35b1449bd9d9222096fd9483ed1f7afb7730c
ozabell <kidlatmc@gmail.com>
2020-04-06 10:40:20 -0700

    Setting up cs1430 repo

---
+++ labs/array.cpp
@@ -0,0 +1,22 @@
+// Isabel Ovalles
+// array.cpp
+// 4/17/2020
+
+// Purpose: Create a two dim array of integers based on user input. 
+
+// Input:
+
+// Prosessing:
+
+// Output:
+
+// Example:
+
+
+#include <iostream>
+
+int main()
+{
+
+  return 0;
+}
