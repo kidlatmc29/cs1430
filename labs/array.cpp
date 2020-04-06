ce262bd49b22d78521b810564510c9d80bb161e1
ozabell <kidlatmc@gmail.com>
2020-04-06 15:00:26 -0700

    finished documentation

--- labs/array.cpp
+++ labs/array.cpp
@@ -2,15 +2,28 @@
 // array.cpp
 // 4/17/2020
 
-// Purpose: Create a two dim array of integers based on user input. 
+// Purpose: Create a two dim array of integers based on user input. The program
+//  will then ask the user to input values for each indeces of the arry. Then,
+//  print out the rows and cols of the array to the screen while also printing
+//  the sum of each column.
 
-// Input:
+// Input: Two integers given by the user via the console.
 
-// Prosessing:
+// Prosessing: Take the two integers given by the user to declare a two dim
+//  array of integers. The sum of each column will also be computed once the
+//  array has been initialized with user given values.
 
-// Output:
+// Output: The end result will be a table of the 2 dim array, with the sum of
+//  both the cols and rows.
 
-// Example:
+// Examples: If the user asks to create a 2 x 2 array, and only gives the
+//  values of 2 in all of the indeces, then when printed it would look like so:  
+//   2   2
+//   2   2
+//  Sum of row 1 is 4
+//  Sum of row 2 is 4
+//  Sum of col 1 is 4
+//  Sum of col 2 is 4
 
 
 #include <iostream>
