0dacbaf51ee77a2ed27b1b705e7d2eef2823a8a4
ozabell <kidlatmc@gmail.com>
2020-04-06 16:03:19 -0700

    Learned how to use dma for 2d arrays

--- labs/array.cpp
+++ labs/array.cpp
@@ -3,7 +3,7 @@
 // 4/17/2020
 
 // Purpose: Create a two dim array of integers based on user input. The program
-//  will then ask the user to input values for each indeces of the arry. Then,
+//  will then ask the user to input values for each indeces of the array. Then,
 //  print out the rows and cols of the array to the screen while also printing
 //  the sum of each column.
 
@@ -17,7 +17,7 @@
 //  both the cols and rows.
 
 // Examples: If the user asks to create a 2 x 2 array, and only gives the
-//  values of 2 in all of the indeces, then when printed it would look like so:  
+//  values of 2 in all of the indeces, then when printed it would look like so:
 //   2   2
 //   2   2
 //  Sum of row 1 is 4
@@ -25,11 +25,46 @@
 //  Sum of col 1 is 4
 //  Sum of col 2 is 4
 
-
 #include <iostream>
+
+using namespace std;
+
+const char SPACE = ' ';
 
 int main()
 {
+  int rowLength;
+  int colLength;
 
+  cout << endl << endl;
+
+  cout << "Please enter a row length: ";
+  cin >> rowLength;
+
+  cout << "Please enter a column length: ";
+  cin >> colLength;
+
+
+
+  int* arr = new int[rowLength * colLength];
+
+  for(int row = 0; row < rowLength; row++) {
+    for(int col = 0; col < colLength; col++) {
+        cout << "Input a number at row " << row << ", col " << col << ": ";
+        cin >> *(arr + row*colLength + col);
+    }
+  }
+
+  // Prints the 2 dim array and the sums of all its rows and cols.
+  for(int row = 0; row < rowLength; row++) {
+    for(int col = 0; col < colLength; col++) {
+      cout << *(arr + row*colLength + col) << SPACE;
+    }
+    cout << endl;
+  }
+
+  cout << endl << endl;
+
+  delete[] arr;
   return 0;
 }
