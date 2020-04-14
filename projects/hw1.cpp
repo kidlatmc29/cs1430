79dc55b10c3065b282466be9d96c1fd838b2d23a
ozabell <kidlatmc@gmail.com>
2020-04-14 12:14:22 -0700

    continuing to work on hw1, fleshing out menu and fxns

--- projects/hw1.cpp
+++ projects/hw1.cpp
@@ -22,8 +22,8 @@ using namespace std;
 struct TeamInfo
 {
   string city;
-  string teamName;
-  string teamMascot;
+  string name;
+  string mascot;
   string confDiv;
   int win;
   int losses;
@@ -37,17 +37,23 @@ const string TEAM = "TEAM";
 const string CONF = "Conf/Div";
 const string WINS = "Wins";
 const string LOSSES = "Losses";
-const string WINPERC = "Winning%";
-
-//void readFile(string fileName);
+const string WIN_PERC = "Winning%";
+const int PRINT_ALL = 1;
+const int SORT_WIN_PERC = 2;
+const int PRINT_DIV = 3;
+const int QUIT = 4;
 
 void welcome();
+
+int getMenuChoice();
+
+void readFile(string fileName);
 
 //void printTeam(TeamInfo[] list);
 
 //void sortWinningPercent(TeamInfo[] list);
 
-//void printAll(TeamInfo[] list);
+void printAll(TeamInfo[] list);
 
 //void printDivision(TeamInfo[] list);
 
@@ -55,21 +61,22 @@ void goodbye();
 
 int main()
 {
-  int choice = 0;
+  int menuChoice = 0;
 
   welcome();
 
-while(choice == 0) {
-  cout << "[MENU]" << endl
-       << "  1 - Display all teams and their winning percentage" << endl
-       << "  2 - Display individual division standings" << endl
-       << "  3 - Sort teams by winning percentage" << endl
-       << "  4 - Quit";
-  cout << endl << "Please choose a selection: ";
-  cin >> choice;
+while(menuChoice != QUIT) {
+  menuChoice = getMenuChoice();
 
-
-
+  switch(menuChoice)
+  {
+    case PRINT_ALL :
+    {
+      printAll();
+      menuChoice = getMenuChoice();
+      break;
+    }
+  }
 }
 
   goodbye();
@@ -80,6 +87,36 @@ void welcome()
 {
   cout << endl << endl << "Welcome!" << endl >> endl;
 
+}
+
+int getMenuChoice()
+{
+  int choice;
+  cout << "[MENU]" << endl
+       << "  1 - Display all teams and their winning percentage" << endl
+       << "  2 - Display individual division standings" << endl
+       << "  3 - Sort teams by winning percentage" << endl
+       << "  4 - Quit";
+  cout << endl << "Please choose a selection: ";
+  cin >> choice;
+
+  return choice;
+}
+
+bool readFile(string fileName)
+{
+  ifstream inFile;
+
+  inFile.open(fileName);
+
+  if(!inFile.fail()) {
+      // read in the files
+  }
+}
+
+void printAll()
+{
+  cout << " in print all fxn" << endl;
 }
 
 void goodbye()
