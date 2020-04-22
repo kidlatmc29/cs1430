754c30bdbfff095289c97afc0cf5242d47b185b1
ozabell <kidlatmc@gmail.com>
2020-04-22 13:14:34 -0700

    fixing up lab week 3

--- labs/week3.cpp
+++ labs/week3.cpp
@@ -11,12 +11,15 @@ int main()
 
   cout << endl << endl;
   cout << "Welcome!" << endl << endl;
-
   cout << "Creating a Car object....." << endl;
   Car *myCar = new Car(1993, "Honda");
-  cout << "myCar is a " << myCar->getyearModel() << " " << myCar->getMake() << endl;
-  cout << endl;
   
+  cout << "myCar is a " << myCar->getyearModel() << " " << myCar->getMake()
+       << endl;
+  cout << "julesCar is " << julesCar.getyearModel() << " " << julesCar.getMake()
+       << endl;
+  cout << endl;
+
   cout << "Accelerating myCar: " << endl;
   for (int i = 0; i < 5; i++) {
     myCar->accelerate();
@@ -30,6 +33,9 @@ int main()
     myCar->brake();
     cout << "Speed is " << myCar->getSpeed() << endl;
   }
+
+  delete myCar;
+  myCar = nullptr;
 
   cout << endl << endl;
   cout << "Goodbye! ";
