cdcd4a08425c448eb4c926825f4098c881c3ce48
ozabell <kidlatmc@gmail.com>
2020-04-22 09:27:43 -0700

    write declarations of Car members in .h file

--- projects/Car.h
+++ projects/Car.h
@@ -1,2 +1,19 @@
 // Isabel Ovalles
 // Car.h
+
+using namespace std;
+
+class Car {
+  private:
+    int yearModel;
+    string make;
+    int speed;
+
+  public:
+      int getyearModel();
+      string getMake();
+      string getSpeed();
+
+      void accelerate(int speed);
+      void brake(int speed);
+}
