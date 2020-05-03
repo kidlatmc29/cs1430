048ab67950eba22f349ede506a5c2cd2379d5458
ozabell <kidlatmc@gmail.com>
2020-05-03 14:50:12 -0700

    trying to debug issue with not being able to inter any 3

--- labs/TicTacToe.cpp
+++ labs/TicTacToe.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 TicTacToe::TicTacToe()
 {
-  cout << "Inside constructor " << endl;
   for(int r = 0; r < ROW; r++) {
     for(int c = 0; c < COL; c++) {
       board[r][c] = BLANK;
@@ -56,24 +55,28 @@ void TicTacToe::placePiece(int player)
   cout << "Enter a row and col: ";
   cin >> row >> col;
 
+
   while(!(validSpace(row, col))) {
     cout << "Enter a row and col: ";
     cin >> row >> col;
   }
 
-  if(player == 1) {
+  if(validSpace(row, col) && player == 1) {
     board[row - 1][col - 1] = X;
-  } else {
+  }
+
+  if(validSpace(row, col) && player == 2) {
     board[row - 1][col - 1] = O;
   }
+
 }
 
 bool TicTacToe::validSpace(int row, int col)
 {
-  if(((row - 1) < ROW) && ((row - 1) > -1)) {
-    if(((col - 1) < COL) && ((col - 1) > -1)) {
+  if(((row - 1) <= ROW) && ((row - 1) > -1)) {
+    if(((col - 1) <= COL) && ((col - 1) > -1)) {
       if(board[row][col] == BLANK) {
-      return true;
+        return true;
       }
     }
   }
@@ -98,6 +101,7 @@ char TicTacToe::checkWinner()
   winner = threeInARow(board[0][0], board[1][1], board[2][2]);
   winner = threeInARow(board[0][2], board[1][1], board[2][0]);
 
+  cout << "winner is = " << winner << endl;
   return winner;
 }
 
